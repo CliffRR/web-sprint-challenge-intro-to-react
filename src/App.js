23729596cd1e76461b22f0d5bcfcc06315b8eebb
unknown <cliff.reinoso98@yahoo.com>
2022-07-25 19:52:45 -0400

    Completed Sprint

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import Character from "./components/Character";
+
+
 
 const App = () => {
+  const [starWarsData, setStarWarsData] = useState([]);
+  
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -8,9 +14,30 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  useEffect(() => {
+    axios.get("https://swapi.dev/api/people/")
+      .then(res => {
+        setStarWarsData(res.data)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+    }, [])
+
+    console.log(starWarsData);
+
+
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <div className = "Background">
+      {
+        starWarsData.map((name)=> {
+          return <Character name = {name.name} year = {name.birth_year} key={name.name} />
+        })
+      }
+      </div>
+
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,49 @@
 // Write your Character component here
+import React from 'react';
+import styled from "styled-components";
+
+
+const StyledBackground = styled.div`
+padding: 5px;
+background-color: rgb(201, 76, 76, .3);
+margin-left:400px;
+margin-right:400px;
+`
+
+const StyledContainer = styled.div`
+display: flex; 
+justify-content: space-between;
+padding: 5px;
+border: 1px solid black;
+`
+
+const StyledYear = styled.div`
+color: ${pr => pr.theme.primaryColor};
+font-size: 20px;
+padding-top: 5px;
+`
+
+const StyledCharacter = styled.div`
+color: ${pr => pr.theme.secondaryColor};
+font-weight: bold;
+font-size: 25px;
+`
+
+
+export default function Character({name, year}){
+return (
+    <StyledBackground className = "background">
+
+    <StyledContainer >
+    <StyledCharacter className = 'character' > 
+        {name}
+    </StyledCharacter>
+
+    <StyledYear className = 'birth'>
+        {year}
+    </StyledYear>
+    </StyledContainer>
+
+    </StyledBackground>
+    )
+  }
